979193f7bb7b133b88bb6c270de54fb86b75c00f
ganfer <100222172+ganfer@users.noreply.github.com>
2025-08-21 20:44:27 +0200

    Create access_denied.php

---
+++ access_denied.php
@@ -0,0 +1,18 @@
+<?php
+http_response_code(403);
+?>
+<!DOCTYPE html>
+<html lang="de">
+<head>
+    <meta charset="UTF-8">
+    <title>Zugriff verweigert</title>
+    <link rel="stylesheet" href="css/style.css">
+</head>
+<body>
+    <div class="error-page">
+        <h1>🚫 Zugriff verweigert</h1>
+        <p>Bitte nutze den QR Code, um den Inhalt dieser Seite zu sehen.</p>
+        <a href="https://google.de"><button>Zu google</button></a>
+    </div>
+</body>
+</html>
