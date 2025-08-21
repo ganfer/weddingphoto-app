40003ab7cd9e64e73974400df1108ab4c5edcb60
ganfer <100222172+ganfer@users.noreply.github.com>
2025-08-21 20:41:28 +0200

    Update config.php

--- config/config.php
+++ config/config.php
@@ -1,3 +1,8 @@
 <?php
 // Dauerhafter Zugriffstoken
 define('ACCESS_TOKEN', 'YOURACCESTOKEN');
+
+//Nextcloud WebDAV
+define('NEXTCLOUD_URL', 'https://nextcloud.XXX.de/remote.php/dav/files/XXX/Photos/');
+define('NEXTCLOUD_USER', 'XXX');
+define('NEXTCLOUD_PASS', 'XXX');
